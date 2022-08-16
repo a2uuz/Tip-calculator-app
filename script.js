da8a2245c108c08d228dedc950f43cd2fe99f1b1
Aziz Moe <66800426+a2uuz@users.noreply.github.com>
2022-08-16 23:46:18 +0300

    update1

---
+++ script.js
@@ -0,0 +1,10 @@
+const form = document.querySelector('form');
+const bill = document.getElementById('bill');
+const percent = document.querySelectorAll(".btns button");
+const custom = document.querySelectorAll(".btns #custom");
+const nop = document.getElementById('nop');
+const tipAmount = document.getElementById("tip-amount");
+const total = document.getElementById("total");
+const reset = document.getElementById("reset");
+
+
