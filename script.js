2d07b84e80ac42aa44b3c7e9eb26af3d78c6ba61
Aziz Moe <66800426+a2uuz@users.noreply.github.com>
2022-08-19 23:18:09 +0300

    update2

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
-const form = document.querySelector('form');
-const bill = document.getElementById('bill');
-const percent = document.querySelectorAll(".btns button");
-const custom = document.querySelectorAll(".btns #custom");
-const nop = document.getElementById('nop');
+const form = document.querySelector("form");
+const bill = document.getElementById("bill");
+const percent = document.querySelectorAll(".btn");
+const custom = document.querySelector("#custom");
+const nop = document.getElementById("nop");
 const tipAmount = document.getElementById("tip-amount");
 const total = document.getElementById("total");
 const reset = document.getElementById("reset");
+const wrapper = document.querySelector(".wrapper");
+
+wrapper.addEventListener("keyup", () => {
+  billInput = Number(bill.value);
+  customInput = Number(custom.value);
+  nopInput = Number(nop.value);
+
+});
+
+function output() {
+  console.log(this.value);
+}
+
+percent.forEach((p) => {
+  p.addEventListener("click", output, true);
+});
+
+
 
 
