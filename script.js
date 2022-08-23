944161cee9b4f9b879b1ba61127279d8f41511c2
Aziz Moe <66800426+a2uuz@users.noreply.github.com>
2022-08-23 23:39:56 +0300

    update4

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const form = document.querySelector("form");
 const bill = document.getElementById("bill");
-const percent = document.querySelector(".btn");
-const custom = document.querySelector("#custom");
+const tipPercent = document.querySelector("#btns");
+const customPercent = document.querySelector("#custom");
 const nop = document.getElementById("nop");
 const tipAmount = document.getElementById("tip-amount");
 const total = document.getElementById("total");
@@ -11,26 +11,24 @@ const error = document.querySelector(".error");
 
 wrapper.addEventListener("keyup", () => {
   billInput = parseFloat(bill.value);
-  customInput = parseFloat(custom.value);
+  customInput = parseFloat(customPercent.value) / 100;
   nopInput = parseFloat(nop.value);
   ACTION();
 });
 
+
 function ACTION() {
   if (nopInput >= 1) {
     let tipCalc = (billInput * customInput) / nopInput;
-    let totalCalc = (billInput + tipCalc) / nopInput;
+    let totalCalc = (billInput + billInput * customInput) / nopInput;
+
     tipAmount.innerHTML = `$${tipCalc.toFixed(2)}`;
     total.innerHTML = `$${totalCalc.toFixed(2)}`;
-  } else {
-    
   }
 }
 
-
 reset.addEventListener("click", () => {
   bill.value = "";
-  custom.value = "";
+  customPercent.value = "";
   nop.value = "";
 });
-
