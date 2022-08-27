eaae5ade24b6c0a2bb4ec7e37ef456ea1ba769a9
Aziz Moe <66800426+a2uuz@users.noreply.github.com>
2022-08-27 23:49:04 +0300

    update5

--- script.js
+++ script.js
@@ -1,32 +1,52 @@
-const form = document.querySelector("form");
 const bill = document.getElementById("bill");
-const tipPercent = document.querySelector("#btns");
+const tipPercent = document.querySelectorAll(".btns .btn");
 const customPercent = document.querySelector("#custom");
 const nop = document.getElementById("nop");
 const tipAmount = document.getElementById("tip-amount");
 const total = document.getElementById("total");
 const reset = document.getElementById("reset");
 const wrapper = document.querySelector(".wrapper");
-const error = document.querySelector(".error");
+const error = document.querySelectorAll(".error .nop");
+let tipValue = 0.15;
 
 wrapper.addEventListener("keyup", () => {
   billInput = parseFloat(bill.value);
-  customInput = parseFloat(customPercent.value) / 100;
   nopInput = parseFloat(nop.value);
+
   ACTION();
 });
 
+// CUSTOM INPUT FIELD FUNCTION
+customPercent.addEventListener("input", CUSTOMINPUT);
+function CUSTOMINPUT() {
+  tipValue = parseFloat(customPercent.value / 100);
+}
 
+// TIP PERCENT BUTTONS FUNCTION
+tipPercent.forEach((val) => {
+  val.addEventListener("click", TIPBTN);
+});
+function TIPBTN(event) {
+  tipPercent.forEach((val) => {
+    if (event.target.innerHTML === val.innerHTML) {
+      tipValue = parseFloat(val.innerHTML) / 100;
+    }
+  });
+  ACTION();
+}
+
+// CALCULATE FUNCTION
 function ACTION() {
   if (nopInput >= 1) {
-    let tipCalc = (billInput * customInput) / nopInput;
-    let totalCalc = (billInput + billInput * customInput) / nopInput;
+    let tipCalc = (billInput * tipValue) / nopInput;
+    let totalCalc = (billInput + billInput * tipValue) / nopInput;
 
     tipAmount.innerHTML = `$${tipCalc.toFixed(2)}`;
     total.innerHTML = `$${totalCalc.toFixed(2)}`;
   }
 }
 
+// RESET BUTTON
 reset.addEventListener("click", () => {
   bill.value = "";
   customPercent.value = "";
